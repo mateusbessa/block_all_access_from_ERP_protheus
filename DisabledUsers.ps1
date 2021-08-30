6944df85dfe34c40b8b8afd6378b6934e7ad90b3
Mateus Bessa <34488734+mateusbessa@users.noreply.github.com>
2021-08-30 14:01:00 -0300

    Add files via upload

---
+++ DisabledUsers.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module ActiveDirectory
+Import-Csv "C:\Windows\SYSVOL\sysvol\mondelli.local\scripts\Inativacao_AD\funcionarios.txt" | ForEach-Object { $samAccountName = $_."samAccountName"
+Get-ADUser -Identity $samAccountName | Disable-ADAccount
+}
